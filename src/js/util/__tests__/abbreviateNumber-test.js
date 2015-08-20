deae069205df479179aaad29c5f13b76ef04e39e
echenley <evan.henley@gmail.com>
2015-08-19 21:10:14 -0500

    update test syntax

--- src/js/util/__tests__/abbreviateNumber-test.js
+++ src/js/util/__tests__/abbreviateNumber-test.js
@@ -11,14 +11,14 @@ describe('abbreviateNumber', function() {
     });
 
     it('abbreviates numbers correctly', function() {
-        expect(abbreviateNumber(0, 2), '0');
-        expect(abbreviateNumber(12, 1), '12');
+        expect(abbreviateNumber(0, 2)).toBe('0');
+        expect(abbreviateNumber(12, 1)).toBe('12');
         expect(abbreviateNumber(1000, 1)).toBe('1k');
-        expect(abbreviateNumber(1234, 0), '1k');
-        expect(abbreviateNumber(34567, 2), '34.57k');
-        expect(abbreviateNumber(918395, 1), '918.4k');
-        expect(abbreviateNumber(2134124, 2), '2.13m');
-        expect(abbreviateNumber(47475782130, 2), '47.48b');
-        expect(abbreviateNumber(47475782130000, 2), '47.48t');
+        expect(abbreviateNumber(1234, 0)).toBe('1k');
+        expect(abbreviateNumber(34567, 2)).toBe('34.57k');
+        expect(abbreviateNumber(918395, 1)).toBe('918.4k');
+        expect(abbreviateNumber(2134124, 2)).toBe('2.13m');
+        expect(abbreviateNumber(47475782130, 2)).toBe('47.48b');
+        expect(abbreviateNumber(47475782130000, 2)).toBe('47.48t');
     });
 });
