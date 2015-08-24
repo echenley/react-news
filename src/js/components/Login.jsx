5f09de0edbe957fc39d22975327aba430373426c
echenley <evan.henley@gmail.com>
2015-08-23 23:24:58 -0500

    move user data to prop for ease of testing

--- src/js/components/Login.jsx
+++ src/js/components/Login.jsx
@@ -1,22 +1,18 @@
 'use strict';
 
 import React from 'react/addons';
-import Reflux from 'reflux';
 
 import Actions from '../actions/Actions';
-import UserStore from '../stores/UserStore';
 
 import Spinner from '../components/Spinner';
+
+const { findDOMNode } = React;
 
 const Login = React.createClass({
 
     propTypes: {
         errorMessage: React.PropTypes.string
     },
-
-    mixins: [
-        Reflux.listenTo(UserStore, 'loginComplete')
-    ],
 
     getInitialState() {
         return {
@@ -25,40 +21,32 @@ const Login = React.createClass({
     },
 
     componentWillReceiveProps(nextProps) {
-        if (nextProps === this.props) {
-            return;
+        if (nextProps.user !== this.props.user) {
+            // clear form if user prop changes (i.e. logged in)
+            this.clearForm();
         }
 
-        React.findDOMNode(this.refs.submit).disabled = false;
+        // allow resubmission if error comes through
         this.setState({
             submitted: false
         });
     },
 
-    loginComplete() {
-        // reset form
-        React.findDOMNode(this.refs.email).value = '';
-        React.findDOMNode(this.refs.password).value = '';
-        React.findDOMNode(this.refs.submit).disabled = false;
-
-        this.setState({
-            submitted: false
-        });
+    clearForm() {
+        findDOMNode(this.refs.email).value = '';
+        findDOMNode(this.refs.password).value = '';
     },
 
     login(e) {
         e.preventDefault();
-
-        // disable submit button
-        React.findDOMNode(this.refs.submit).disabled = true;
 
         this.setState({
             submitted: true
         });
 
         Actions.login({
-            email: React.findDOMNode(this.refs.email).value.trim(),
-            password: React.findDOMNode(this.refs.password).value.trim()
+            email: findDOMNode(this.refs.email).value.trim(),
+            password: findDOMNode(this.refs.password).value.trim()
         });
     },
 
@@ -73,10 +61,10 @@ const Login = React.createClass({
                 <h1>Login</h1>
                 <form onSubmit={ this.login } className="md-form">
                     <label htmlFor="email">Email</label>
-                    <input type="email" placeholder="Email" id="email" ref='email' />
+                    <input type="email" placeholder="Email" id="email" ref="email" />
                     <label htmlFor="password">Password</label>
                     <input type="password" placeholder="Password" id="password" ref="password" />
-                    <button type="submit" className="button button-primary" ref="submit">
+                    <button type="submit" className="button button-primary" ref="submit" disabled={ this.state.submitted }>
                         { this.state.submitted ? <Spinner /> : 'Sign In' }
                     </button>
                 </form>
