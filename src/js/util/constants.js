371ffa28ed7b9b56825b7134ca5c57fded33ae6e
echenley <evan.henley@gmail.com>
2015-08-29 19:19:22 -0500

    add NO_COMMENT constant

--- src/js/util/constants.js
+++ src/js/util/constants.js
@@ -5,6 +5,7 @@ const errorMessages = {
     INVALID_EMAIL: 'Invalid email address.',
     INVALID_PASSWORD: 'Invalid password.',
     INVALID_USER: 'User doesn\'t exist.',
+    NO_COMMENT: 'You have to enter a comment.',
     NO_USERNAME: 'You have to enter a username.',
     EMAIL_TAKEN: 'That email is taken.',
     USERNAME_TAKEN: 'That username is taken.',
