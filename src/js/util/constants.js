34e5262b7eaa8dff6c6e23aa467dd4556c0b7578
echenley <evan.henley@gmail.com>
2015-08-31 18:30:21 -0500

    add COMMENT_FAILED error

--- src/js/util/constants.js
+++ src/js/util/constants.js
@@ -5,6 +5,7 @@ const errorMessages = {
     INVALID_EMAIL: 'Invalid email address.',
     INVALID_PASSWORD: 'Invalid password.',
     INVALID_USER: 'User doesn\'t exist.',
+    COMMENT_FAILED: 'Comment submission failed. Try again.',
     NO_COMMENT: 'You have to enter a comment.',
     NO_USERNAME: 'You have to enter a username.',
     EMAIL_TAKEN: 'That email is taken.',
