f5e3796dda4a9abf4be2d0824b6dd1257deb227f
echenley <evan.henley@gmail.com>
2015-10-04 20:50:50 -0500

    let > const

--- src/js/components/Comment.jsx
+++ src/js/components/Comment.jsx
@@ -5,7 +5,6 @@ import Actions from '../actions/Actions';
 
 import { Link } from 'react-router';
 import Upvote from './Upvote';
-import abbreviateNumber from '../util/abbreviateNumber';
 import timeAgo from '../util/timeAgo';
 
 const Comment = React.createClass({
@@ -17,27 +16,22 @@ const Comment = React.createClass({
     },
 
     render() {
-        let {
-            user,
-            comment,
-            showPostTitle
-        } = this.props;
+        const { user, comment, showPostTitle } = this.props;
+        const userUpvoted = user.upvoted || {};
 
-        let userUpvoted = user.upvoted || {};
-
-        let postLink = showPostTitle && (
+        const postLink = showPostTitle && (
             <span className="post-info-item post-link">
                 <Link to={ `/post/${comment.postId}` }>{ comment.postTitle }</Link>
             </span>
         );
 
-        let deleteOption = user.uid === comment.creatorUID && (
+        const deleteOption = user.uid === comment.creatorUID && (
             <span className="delete post-info-item">
                 <a onClick={ () => Actions.deleteComment(comment) }>delete</a>
             </span>
         );
 
-        let upvoteActions = {
+        const upvoteActions = {
             upvote: Actions.upvoteComment,
             downvote: Actions.downvoteComment
         };
